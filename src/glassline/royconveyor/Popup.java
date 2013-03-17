867676baeebe163505ad080aade8ab4ef3ee1937
Roy Zheng <roycraft3@gmail.com>
2013-03-16 21:38:29 -0700

    added frame for popup

--- src/glassline/royconveyor/Popup.java
+++ src/glassline/royconveyor/Popup.java
@@ -4,15 +4,42 @@ package glassline.royconveyor;
 Object representation of the conveyor line pop up system. has an OperatorAgent attached. 
  */
 public class Popup{
+    
+    protected BinaryTree<Barcode> persistentgrab;///<grabs all parts with the specified barcode
+    protected BinaryTree<UniqueObject> singlegrab;///<grabs a single part
 
-protected BinaryTree<
-protected BinaryTree
-
-    public void msgGlassRequest(Glass g)
+    /**
+sets a part with the specified barcode to always be grabbed
+     */
+    public void addPersistent(Barcode b)
     {
 
     }
-    public void msgSensorTrigger(Glass g)
+    /**
+stop the part with the specified barcode from being always grabbed
+     */
+    public void removePersistent(Barcode b)
+    {
+
+    }
+    /**
+grab the passed UniqueObject once
+     */
+    public void addUnique(UniqueObject o)
+    {
+
+    }
+    /**
+cancel order to grab the passed UniqueObject once
+    */
+public void removeUnique(UbiqueObject o)
+    {
+
+    }
+    /**
+check if the part is to be grabbed
+     */
+    public boolean needsGrab(Glass g)
     {
 
     }
