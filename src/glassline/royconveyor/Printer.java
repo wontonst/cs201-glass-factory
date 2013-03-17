89be13144d795cdefdd1e4396b754e43a1bc7c18
Roy Zheng <roycraft3@gmail.com>
2013-03-16 21:55:02 -0700

    printer

---
+++ src/glassline/royconveyor/Printer.java
@@ -0,0 +1,16 @@
+package glassline.royconveyor;
+
+public class Printer implements PrintControllable{
+
+    private static boolean verbosity = true;///<true = print ; false = disable printing
+
+    public void setPrintOutput(boolean in)
+    {
+	this.verbosity = in;
+    }
+    public void print(String s)
+    {
+	if(verbosity)
+	System.out.println("RoyFamily"+s);
+    }
+}
