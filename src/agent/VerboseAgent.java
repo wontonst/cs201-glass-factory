ccf91303503965a2f6b16549366f92c83cd68a61
Roy Zheng <roycraft3@gmail.com>
2013-03-17 20:38:57 -0700

    verboseagent

---
+++ src/agent/VerboseAgent.java
@@ -0,0 +1,19 @@
+
+public class VerboseAgent{
+    private String name = null;
+    private Printer printer = new Printer();
+    public VerboseObject(){}
+    public VerboseObject(String name)
+    {
+	this.name = name;
+    }
+    public print(String s)
+    {
+	if(this.name != null)
+	    {
+		printer.print("."+this.name+": "+s);
+		return;
+	    }
+	printer.print("."+this.getClassName()+": "+s);
+    }
+}
